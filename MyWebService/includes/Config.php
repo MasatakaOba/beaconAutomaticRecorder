b526a07139918f6143185f8e29e4afa735fd0fda
MasatakaOba <masata_828@icloud.com>
2019-05-19 12:20:01 +0900

    Update 2018/05/19

---
+++ MyWebService/includes/Config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_USERNAME', 'AAAAAA');
+define('DB_PASSWORD', 'BBBBBB');
+define('DB_HOST', 'CCCCCC');
+define('DB_NAME', 'DDDDDD');
+?>
---
+++ MyWebService/includes/DbOperation.php
@@ -0,0 +1,32 @@
+<?php
+
+class DbOperation
+{
+    private $conn;
+
+    //Constructor
+    function __construct()
+    {
+        require_once dirname(__FILE__) . '/Config.php';
+        require_once dirname(__FILE__) . '/DbConnect.php';
+        // opening db connection
+        $db = new DbConnect();
+        $this->conn = $db->connect();
+    }
+
+    //Function to create a new user
+    public function beacon($date, $flag)
+    {
+        $stmt = $this->conn->prepare("INSERT INTO beacon(date, flag) values(?, ?)");
+        $stmt->bind_param("si", $date, $flag);
+        $result = $stmt->execute();
+        $stmt->close();
+        if ($result) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+?>
